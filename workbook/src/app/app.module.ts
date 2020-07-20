792026e223629ba3bb9e6440bea3c0f03ba1ef28
Drawcard <git@drawcard.com.au>
2020-07-20 21:31:40 +0930

    Remove material shared module

--- workbook/src/app/app.module.ts
+++ workbook/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MaterialSharedModule } from './material-shared-module';
 import { SharedComponent } from './shared/shared.component';
 
 @NgModule({
