4962892227c227cc348c77fe0d4a59064327bf36
Drawcard <git@drawcard.com.au>
2020-07-20 21:33:06 +0930

    Refactor nav menu

--- workbook/src/app/app.module.ts
+++ workbook/src/app/app.module.ts
@@ -6,17 +6,30 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { SharedComponent } from './shared/shared.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SharedComponent
+    SharedComponent,
+    MainMenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialSharedModule
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
