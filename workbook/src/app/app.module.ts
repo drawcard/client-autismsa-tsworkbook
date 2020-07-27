04143e198fbbb8bbf1187c749bb621c8afaa113a
Drawcard <git@drawcard.com.au>
2020-07-27 17:55:26 +0930

    Implement reactive form

--- workbook/src/app/app.module.ts
+++ workbook/src/app/app.module.ts
@@ -18,7 +18,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatCardModule } from '@angular/material/card';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -28,6 +28,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     WorkbookFormComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
--- workbook/src/app/workbook-form/workbook-form.component.ts
+++ workbook/src/app/workbook-form/workbook-form.component.ts
@@ -1,22 +1,19 @@
 import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-workbook-form',
   templateUrl: './workbook-form.component.html',
   styleUrls: ['./workbook-form.component.scss']
 })
+
 export class WorkbookFormComponent {
-  formContent = this.fb.group({
-    b1_q1: [null, Validators.required],
-    b1_q2: [null, Validators.required],
-    b1_q3: [null, Validators.required],
-    b1_q4: [null, Validators.required]
-  });
+  b1_q1 = new FormControl('');
+  b1_q2 = new FormControl('');
+  b1_q3 = new FormControl('');
+  b1_q4 = new FormControl('');
 
-  constructor(private fb: FormBuilder) { }
-
-  onSubmit() {
-    alert('Thanks!');
+  formComplete() {
+    console.table(this);
   }
 }
