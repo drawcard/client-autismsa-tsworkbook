9e5d01e4f18e75fc4ecd500e73097f031ea72eb2
Drawcard <git@drawcard.com.au>
2020-08-05 18:55:22 +0930

    Update dist folder

--- docs/assets/img/README.md
+++
@@ -1,7 +0,0 @@
-# Note
-
-If you update files in this directory, be sure to convert them
-to base64 (https://www.base64-image.de/) and update the mindMap001,002,003,004 variables in:
-workbook/src/app/workbook-form/workbook-form.component.ts
-
-More: https://github.com/bpampuch/pdfmake/issues/90
--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -10,12 +10,14 @@ import { map, shareReplay } from 'rxjs/operators';
 })
 export class MainMenuComponent {
 
+  menuBaseURL: string = 'https://drawcard.com.au/autismsa';
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
 }
