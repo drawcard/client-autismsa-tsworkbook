bafb93c2c7d477b06fac8f1005e3062c44c8be5c
Drawcard <git@drawcard.com.au>
2020-09-07 00:57:07 +0930

    Add parser headers

--- src/assets/parser.php
+++ src/assets/parser.php
@@ -2,8 +2,12 @@
 
 // This PHP file is a helper for the web app found at: thespectrum.org.au/myautism
 
+
 // Set headers
- header('Content-Type: application/javascript');
+header('Content-Type: application/javascript');
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, OPTIONS');
+
  // Website path to content file
  $data = file_get_contents('https://thespectrum.org.au/myautism-data/content.json?callback=ng_jsonp_callback_0');
  // Create JSONP response
