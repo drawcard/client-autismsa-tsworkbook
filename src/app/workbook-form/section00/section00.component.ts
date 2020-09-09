c1d9e8f5d6d0ae4ab242ee83c1a3f9757861eaea
Drawcard <git@drawcard.com.au>
2020-09-09 20:21:50 +0930

    Revert content move on Section 0 & 1

--- src/app/workbook-form/section00/section00.component.ts
+++ src/app/workbook-form/section00/section00.component.ts
@@ -14,11 +14,7 @@ export class Section00Component implements OnInit {
       // Titles
       title0: ``,
       // Body
-      body0: `My Autism and Me: Planning Booklet includes important information about me! It is a document that will change and develop as I do. It's purpose is to help everyone in my life understand my autism and what it means for me and my family on a day to day basis. <br><br> My Autism and Me: Planning Booklet includes information about: <br><br>
-        • The characteristics of my autism <br>
-        • The impact of those characteristics on me and my family <br>
-        • Strategies that can support me and my family <br>
-        • Information about supports and services <br>`,
+      body0: ``,
       body1: ``,
     }
   ];
--- src/app/workbook-form/section01/section01.component.ts
+++ src/app/workbook-form/section01/section01.component.ts
@@ -25,7 +25,11 @@ export class Section01Component implements OnInit {
       // Titles
       title0: `About Me`,
       // Body
-      body0: ``,
+      body0: `My Autism and Me: Planning Booklet includes important information about me! It is a document that will change and develop as I do. It's purpose is to help everyone in my life understand my autism and what it means for me and my family on a day to day basis. <br><br> My Autism and Me: Planning Booklet includes information about: <br><br>
+        • The characteristics of my autism <br>
+        • The impact of those characteristics on me and my family <br>
+        • Strategies that can support me and my family <br>
+        • Information about supports and services <br>`,
       body1: `It is always helpful for the people in my life to know what I like, what I am good at, what I find challenging and
   the supports I need. This information can help guide decisions around how to interact with me, what I am likely to be
   successful at and what skills I need to continue developing.`,
