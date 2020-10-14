4838caedd77dc764befb0dd5103d71a9aac29873
Drawcard <git@drawcard.com.au>
2020-10-15 01:35:03 +1030

    Add a note about removing redundant file

--- src/app/workbook-form/section00/section00.component.ts
+++ src/app/workbook-form/section00/section00.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
 import { HttpClient } from "@angular/common/http";
-import * as Setting from './../../workbook-settings';
 import ContentFilePaths from '../../../assets/content/content-filepaths.json';
 
 @Component({
--- src/app/workbook-settings.ts
+++ src/app/workbook-settings.ts
@@ -1,4 +1,5 @@
 'use strict';
 
+// TODO: remove this file, it's no longer required
 export const FORM_URL = "https://thespectrum.org.au/myautism/";
 export const CONTENT_URL = "https://thespectrum.org.au/myautism-data/";
