45f1c2481123513a07eeb5bd23a3c39156d6f5a5
Drawcard <git@drawcard.com.au>
2020-10-19 23:18:37 +1030

    Restore missing listchange()

--- src/app/workbook-form/section02/section02.component.ts
+++ src/app/workbook-form/section02/section02.component.ts
@@ -113,4 +113,37 @@ export class Section02Component implements OnInit {
     });
   }
 
+  radioChange(event: MatRadioChange, data) {
+    var obj = this.rad1.filter(x => x.id == data.id)[0];
+    obj.selected = event.value;
+  }
+
+  master_change() {
+    for (let value of Object.values(this.cbl1)) {
+      value.checked = this.master_checked;
+    }
+  }
+
+  list_change() {
+    let checked_count = 0;
+    //Get total checked items
+    for (let value of Object.values(this.cbl1)) {
+      if (value.checked)
+        checked_count++;
+    }
+
+    if (checked_count > 0 && checked_count < this.cbl1.length) {
+      // If some checkboxes are checked but not all; then set Indeterminate state of the master to true.
+      this.master_indeterminate = true;
+    } else if (checked_count == this.cbl1.length) {
+      //If checked count is equal to total items; then check the master checkbox and also set Indeterminate state to false.
+      this.master_indeterminate = false;
+      this.master_checked = true;
+    } else {
+      //If none of the checkboxes in the list is checked then uncheck master also set Indeterminate to false.
+      this.master_indeterminate = false;
+      this.master_checked = false;
+    }
+  }
+
 }
