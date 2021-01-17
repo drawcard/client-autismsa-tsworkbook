4b2fdfdf1f313da1c1720d2b95275b73fc7e6668
Drawcard <signup@drawcard.com.au>
2021-01-17 12:50:33 +1030

    Make all text field data persistent

--- src/app/workbook-form/section05/section05.component.ts
+++ src/app/workbook-form/section05/section05.component.ts
@@ -35,6 +35,63 @@ export class Section05Component implements OnInit {
 
   ngOnInit(): void {
     this.fetchMarkDownContent();
+    this.getLocalStorage();
+    this.setLocalStorage();
+  }
+  setLocalStorage() {
+    this.q1.valueChanges.subscribe(q1_input_value => {
+      localStorage.setItem("s5_q1_input_value", q1_input_value);
+    });
+    this.q2.valueChanges.subscribe(q2_input_value => {
+      localStorage.setItem("s5_q2_input_value", q2_input_value);
+    });
+    this.q3.valueChanges.subscribe(q3_input_value => {
+      localStorage.setItem("s5_q3_input_value", q3_input_value);
+    });
+    this.q4.valueChanges.subscribe(q4_input_value => {
+      localStorage.setItem("s5_q4_input_value", q4_input_value);
+    });
+    this.q5.valueChanges.subscribe(q5_input_value => {
+      localStorage.setItem("s5_q5_input_value", q5_input_value);
+    });
+    this.q6.valueChanges.subscribe(q6_input_value => {
+      localStorage.setItem("s5_q6_input_value", q6_input_value);
+    });
+    this.q7.valueChanges.subscribe(q7_input_value => {
+      localStorage.setItem("s5_q7_input_value", q7_input_value);
+    });
+    this.q8.valueChanges.subscribe(q8_input_value => {
+      localStorage.setItem("s5_q8_input_value", q8_input_value);
+    });
+    this.q9.valueChanges.subscribe(q9_input_value => {
+      localStorage.setItem("s5_q9_input_value", q9_input_value);
+    });
+    this.q10.valueChanges.subscribe(q10_input_value => {
+      localStorage.setItem("s5_q10_input_value", q10_input_value);
+    });
+    this.q11.valueChanges.subscribe(q11_input_value => {
+      localStorage.setItem("s5_q11_input_value", q11_input_value);
+    });
+    this.q12.valueChanges.subscribe(q12_input_value => {
+      localStorage.setItem("s5_q12_input_value", q12_input_value);
+    });
+  }
+
+  getLocalStorage() {
+    if (localStorage.length > 0) {
+      this.q1.setValue(localStorage.getItem("s5_q1_input_value"));
+      this.q2.setValue(localStorage.getItem("s5_q2_input_value"));
+      this.q3.setValue(localStorage.getItem("s5_q3_input_value"));
+      this.q4.setValue(localStorage.getItem("s5_q4_input_value"));
+      this.q5.setValue(localStorage.getItem("s5_q5_input_value"));
+      this.q6.setValue(localStorage.getItem("s5_q6_input_value"));
+      this.q7.setValue(localStorage.getItem("s5_q7_input_value"));
+      this.q8.setValue(localStorage.getItem("s5_q8_input_value"));
+      this.q9.setValue(localStorage.getItem("s5_q9_input_value"));
+      this.q10.setValue(localStorage.getItem("s5_q10_input_value"));
+      this.q11.setValue(localStorage.getItem("s5_q11_input_value"));
+      this.q12.setValue(localStorage.getItem("s5_q12_input_value"));
+    }
   }
 
   fetchMarkDownContent() {
--- src/app/workbook-form/section07/section07.component.ts
+++ src/app/workbook-form/section07/section07.component.ts
@@ -29,6 +29,39 @@ export class Section07Component implements OnInit {
 
   ngOnInit(): void {
     this.fetchMarkDownContent();
+    this.getLocalStorage();
+    this.setLocalStorage();
+  }
+  setLocalStorage() {
+    this.q1.valueChanges.subscribe(q1_input_value => {
+      localStorage.setItem("s7_q1_input_value", q1_input_value);
+    });
+    this.q2.valueChanges.subscribe(q2_input_value => {
+      localStorage.setItem("s7_q2_input_value", q2_input_value);
+    });
+    this.q3.valueChanges.subscribe(q3_input_value => {
+      localStorage.setItem("s7_q3_input_value", q3_input_value);
+    });
+    this.q4.valueChanges.subscribe(q4_input_value => {
+      localStorage.setItem("s7_q4_input_value", q4_input_value);
+    });
+    this.q5.valueChanges.subscribe(q5_input_value => {
+      localStorage.setItem("s7_q5_input_value", q5_input_value);
+    });
+    this.q6.valueChanges.subscribe(q6_input_value => {
+      localStorage.setItem("s7_q6_input_value", q6_input_value);
+    });
+  }
+
+  getLocalStorage() {
+    if (localStorage.length > 0) {
+      this.q1.setValue(localStorage.getItem("s7_q1_input_value"));
+      this.q2.setValue(localStorage.getItem("s7_q2_input_value"));
+      this.q3.setValue(localStorage.getItem("s7_q3_input_value"));
+      this.q4.setValue(localStorage.getItem("s7_q4_input_value"));
+      this.q5.setValue(localStorage.getItem("s7_q5_input_value"));
+      this.q6.setValue(localStorage.getItem("s7_q6_input_value"));
+    }
   }
 
   fetchMarkDownContent() {
