a2014a058c9c88e4bbce6f49c7be25cfb977bf04
Drawcard <>
2021-01-19 21:37:06 +1030

    Working prototype for checkboxes + localStorage

--- src/app/workbook-form/section02/section02.component.ts
+++ src/app/workbook-form/section02/section02.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChildren } from '@angular/core';
 import { MatRadioChange } from '@angular/material/radio';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroupName } from '@angular/forms';
 
 import { HttpClient } from "@angular/common/http";
 import { FetchDataService } from '../services/fetch-data.service';
@@ -32,6 +32,7 @@ export class Section02Component implements OnInit {
     's02-cbl2',
     's02-cbl4'
   ];
+  checkboxStates: { id: number, checked: boolean }[];
 
   cbl1 = [];
   cbl2 = [];
@@ -59,54 +60,54 @@ export class Section02Component implements OnInit {
 
     this.cbl1 = [
       // Thanks: https://www.freakyjolly.com/angular-material-check-uncheck-checkbox-list-with-indeterminate-state-using-matcheckboxmodule/
-      { id: 1, name: "I need a visual support to provide warning of what is next", checked: false },
-      { id: 2, name: "I find it tricky to give eye contact", checked: false },
-      { id: 3, name: "I have trouble knowing what others are going to do e.g. I don’t reach out my arms when someone is about to pick me up, or I don’t seem to understand familiar games e.g. ‘Peek-a-Boo’ or ‘Chasey’", checked: false },
-      { id: 4, name: "I find understanding body language and gestures confusing e.g. smiling when smiled at; pointing or waving in context ", checked: false },
-      { id: 5, name: "I can find it tricky to know how close to stand and sit next to other people ", checked: false },
-      { id: 6, name: "I physically show people what I want or need rather than telling them with words eg pull mum to the fridge when I want a snack or bang on the door if I want to go outside  ", checked: false },
-      { id: 7, name: "I prefer to be alone, with adults or younger children instead of playing with peers ", checked: false },
-      { id: 8, name: "I stick with people and activities I know ", checked: false },
-      { id: 9, name: "I have difficulty imitating actions e.g. following actions in a song or dance", checked: false },
-      { id: 10, name: "I find ‘pretend’ or ‘imaginary’ games confusing and hard to join in with", checked: false },
-      { id: 11, name: "I am confused by other people’s facial expressions", checked: false },
-      { id: 12, name: "I don’t respond to my name when people are talking to me, turn my head to see who is talking or react to loud sounds", checked: false },
-      { id: 13, name: "I find it hard and frustrating to understand what others are saying ", checked: false },
-      { id: 14, name: "I find it hard and frustrating to use my words to get what I need and want from others ", checked: false },
-      { id: 15, name: "I do better when instructions are given to me one at a time ", checked: false },
-      { id: 16, name: "I repeat words and phrases that other people have said (echolalia)", checked: false },
-      { id: 17, name: "I use words and phrases out of context ", checked: false },
-      { id: 18, name: "I keep conversations to topics that I am really interested in", checked: false },
-      { id: 19, name: "I ask questions but don’t make comments (or vice versa) ", checked: false },
-      { id: 20, name: "I find understanding or talking about feelings tricky (mine and others)", checked: false },
-      { id: 21, name: "I have a literal understanding of words e.g. I think “pull your socks up” means I need to pull up my socks rather than try to improve ", checked: false },
+      { id: 1, groupName: "s02-cbl1", label: "I need a visual support to provide warning of what is next", checked: false },
+      { id: 2, groupName: "s02-cbl1", label: "I find it tricky to give eye contact", checked: false },
+      { id: 3, groupName: "s02-cbl1", label: "I have trouble knowing what others are going to do e.g. I don’t reach out my arms when someone is about to pick me up, or I don’t seem to understand familiar games e.g. ‘Peek-a-Boo’ or ‘Chasey’", checked: false },
+      { id: 4, groupName: "s02-cbl1", label: "I find understanding body language and gestures confusing e.g. smiling when smiled at; pointing or waving in context ", checked: false },
+      { id: 5, groupName: "s02-cbl1", label: "I can find it tricky to know how close to stand and sit next to other people ", checked: false },
+      { id: 6, groupName: "s02-cbl1", label: "I physically show people what I want or need rather than telling them with words eg pull mum to the fridge when I want a snack or bang on the door if I want to go outside  ", checked: false },
+      { id: 7, groupName: "s02-cbl1", label: "I prefer to be alone, with adults or younger children instead of playing with peers ", checked: false },
+      { id: 8, groupName: "s02-cbl1", label: "I stick with people and activities I know ", checked: false },
+      { id: 9, groupName: "s02-cbl1", label: "I have difficulty imitating actions e.g. following actions in a song or dance", checked: false },
+      { id: 10, groupName: "s02-cbl1", label: "I find ‘pretend’ or ‘imaginary’ games confusing and hard to join in with", checked: false },
+      { id: 11, groupName: "s02-cbl1", label: "I am confused by other people’s facial expressions", checked: false },
+      { id: 12, groupName: "s02-cbl1", label: "I don’t respond to my name when people are talking to me, turn my head to see who is talking or react to loud sounds", checked: false },
+      { id: 13, groupName: "s02-cbl1", label: "I find it hard and frustrating to understand what others are saying ", checked: false },
+      { id: 14, groupName: "s02-cbl1", label: "I find it hard and frustrating to use my words to get what I need and want from others ", checked: false },
+      { id: 15, groupName: "s02-cbl1", label: "I do better when instructions are given to me one at a time ", checked: false },
+      { id: 16, groupName: "s02-cbl1", label: "I repeat words and phrases that other people have said (echolalia)", checked: false },
+      { id: 17, groupName: "s02-cbl1", label: "I use words and phrases out of context ", checked: false },
+      { id: 18, groupName: "s02-cbl1", label: "I keep conversations to topics that I am really interested in", checked: false },
+      { id: 19, groupName: "s02-cbl1", label: "I ask questions but don’t make comments (or vice versa) ", checked: false },
+      { id: 20, groupName: "s02-cbl1", label: "I find understanding or talking about feelings tricky (mine and others)", checked: false },
+      { id: 21, groupName: "s02-cbl1", label: "I have a literal understanding of words e.g. I think “pull your socks up” means I need to pull up my socks rather than try to improve ", checked: false },
       // Other
     ],
       this.cbl2 = [
         // Thanks: https://www.freakyjolly.com/angular-material-check-uncheck-checkbox-list-with-indeterminate-state-using-matcheckboxmodule/
-        { id: 1, name: "I can often feel frustrated as I am not easily understood ", checked: false },
-        { id: 2, name: "It can be harder or slower to get things done as I need support to follow verbal instructions", checked: false },
-        { id: 3, name: "I need more processing time to respond to verbal information", checked: false },
-        { id: 4, name: "It can be difficult having unfamiliar visitors/carers in the home", checked: false },
-        { id: 5, name: "I need a space in the house that is just mine, for calm down time", checked: false },
-        { id: 6, name: "I may need support during playtime between me and my siblings/friends ", checked: false },
-        { id: 7, name: "I find it helpful when my family and friends adjust their communication style to help me understand ", checked: false },
-        { id: 8, name: "I can find it hard to attend mainstream social activities with my family", checked: false },
-        { id: 9, name: "My extended family and friends can find it hard to understand me ", checked: false },
+      { id: 1, groupName: "s02-cbl2", label: "I can often feel frustrated as I am not easily understood ", checked: false },
+      { id: 2, groupName: "s02-cbl2", label: "It can be harder or slower to get things done as I need support to follow verbal instructions", checked: false },
+      { id: 3, groupName: "s02-cbl2", label: "I need more processing time to respond to verbal information", checked: false },
+      { id: 4, groupName: "s02-cbl2", label: "It can be difficult having unfamiliar visitors/carers in the home", checked: false },
+      { id: 5, groupName: "s02-cbl2", label: "I need a space in the house that is just mine, for calm down time", checked: false },
+      { id: 6, groupName: "s02-cbl2", label: "I may need support during playtime between me and my siblings/friends ", checked: false },
+      { id: 7, groupName: "s02-cbl2", label: "I find it helpful when my family and friends adjust their communication style to help me understand ", checked: false },
+      { id: 8, groupName: "s02-cbl2", label: "I can find it hard to attend mainstream social activities with my family", checked: false },
+      { id: 9, groupName: "s02-cbl2", label: "My extended family and friends can find it hard to understand me ", checked: false },
         // Other
       ],
       this.cbl4 = [
         // Thanks: https://www.freakyjolly.com/angular-material-check-uncheck-checkbox-list-with-indeterminate-state-using-matcheckboxmodule/
-        { id: 1, name: "Parents", checked: false },
-        { id: 2, name: "Other family members", checked: false },
-        { id: 3, name: "Educators (childcare/kindergarten/school)", checked: false },
-        { id: 4, name: "Developmental educators", checked: false },
-        { id: 5, name: "Occupational Therapists", checked: false },
-        { id: 6, name: "Speech Pathologists", checked: false },
-        { id: 7, name: "Behaviour practitioners", checked: false },
-        { id: 8, name: "Psychologists", checked: false },
-        { id: 9, name: "Coaches e.g. swimming coach, gymnastic coach", checked: false },
-        { id: 10, name: "Therapists", checked: false },
+      { id: 1, groupName: "s02-cbl4", label: "Parents", checked: false },
+      { id: 2, groupName: "s02-cbl4", label: "Other family members", checked: false },
+      { id: 3, groupName: "s02-cbl4", label: "Educators (childcare/kindergarten/school)", checked: false },
+      { id: 4, groupName: "s02-cbl4", label: "Developmental educators", checked: false },
+      { id: 5, groupName: "s02-cbl4", label: "Occupational Therapists", checked: false },
+      { id: 6, groupName: "s02-cbl4", label: "Speech Pathologists", checked: false },
+      { id: 7, groupName: "s02-cbl4", label: "Behaviour practitioners", checked: false },
+      { id: 8, groupName: "s02-cbl4", label: "Psychologists", checked: false },
+      { id: 9, groupName: "s02-cbl4", label: "Coaches e.g. swimming coach, gymnastic coach", checked: false },
+      { id: 10, groupName: "s02-cbl4", label: "Therapists", checked: false },
         // Other
       ]
   }
@@ -116,6 +117,7 @@ export class Section02Component implements OnInit {
     this.getLocalStorage();
     this.setLocalStorage();
     this.getCheckbox();
+    this.initCheckbox();
   }
   setLocalStorage() {
     this.other1.valueChanges.subscribe(other1_input_value => {
@@ -156,6 +158,24 @@ export class Section02Component implements OnInit {
     // alert('changed radio!');
   }
 
+  initCheckbox() {
+    this.checkboxNames.forEach(element => {
+      // If localStorage has checkbox values, retrieve and store in this.selection
+      if (localStorage.getItem(`checkboxSelection-${element}`)) {
+        let fetchStorage = JSON.parse(localStorage.getItem(`checkboxSelection-${element}`));
+        // Merge two arrays, so that the first array is updated with changes from the second
+        // https://stackoverflow.com/a/39127724
+        let merged = _.map(
+          _.assign(
+            _.mapKeys(this.cbl1, v => v.id), // first array
+            _.mapKeys(fetchStorage, v => v.id) // second array
+          )
+        );
+        this.cbl1 = merged;
+      }
+    });
+  }
+
   getCheckbox() {
     this.checkboxNames.forEach(element => {
       // If localStorage has checkbox values, retrieve and store in this.selection
@@ -165,19 +185,19 @@ export class Section02Component implements OnInit {
     });
   }
 
-  setCheckbox(id, name, event) {
-    let currentlyChecked = { id: id, checked: event.checked }; // The current id & checked state of the checkbox
+  setCheckbox(id, groupName, label, event) {
+    let currentlyChecked = { id: id, groupName: groupName, label: label, checked: event.checked }; // The current id & checked state of the checkbox
 
     if (event.checked === true) {
       let uniqueValues = [];
       this.selection.push(currentlyChecked); // Update array with current checkbox state
       uniqueValues = _.uniqWith(this.selection, _.isEqual); // De-duplicate entries
       this.selection = uniqueValues;
-      localStorage.setItem(`checkboxSelection-${name}`, JSON.stringify(this.selection)); // Store the array in browser
+      localStorage.setItem(`checkboxSelection-${groupName}`, JSON.stringify(this.selection)); // Store the array in browser
     }
     else {
-      this.deselected = _.pullAllWith(this.selection, [{ id: id, checked: true }], _.isEqual); // Remove deselected checkbox from array
-      localStorage.setItem(`checkboxSelection-${name}`, JSON.stringify(this.selection)); // Store the array in browser
+      this.deselected = _.pullAllWith(this.selection, [{ id: id, groupName: groupName, label: label, checked: true }], _.isEqual); // Remove deselected checkbox from array
+      localStorage.setItem(`checkboxSelection-${groupName}`, JSON.stringify(this.selection)); // Store the array in browser
     }
   }
 }
